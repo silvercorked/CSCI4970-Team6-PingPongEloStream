125460e74605c386fc8fd74508783e01ad05e894
Alex Wissing <awissing123@gmail.com>
2022-10-14 14:03:31 -0600

    removed vagrant files

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,6 @@ use App\Http\Controllers\UserController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 // auth/login/creds routes
 Route::group([], function() {
     Route::post('/login', [AuthController::class, 'getToken'])->name('get user a token via login');
