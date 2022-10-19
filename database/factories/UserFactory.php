c4b3506d33ca575e9978ad1d035b4a02bbb3e7c7
Colin Gregurich <coling112198@gmail.com>
2022-10-18 22:39:09 -0500

    added random profile pic url code so urls are random on seed, but consistent afterwards

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,13 +23,17 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $pic_num = rand(0, 74);
+        $is_male = rand(0, 1) == 1;
+        $gender = $is_male ? 'male' : 'female';
+        $pic_url = "https://xsgames.co/randomusers/assets/avatars/{$gender}/$pic_num.jpg";
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'profile_photo_path' => 'https://xsgames.co/randomusers/avatar.php?g=male'
+            'profile_photo_path' => $pic_url
         ];
     }
 
