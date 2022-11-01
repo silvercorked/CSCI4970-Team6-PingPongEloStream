fe308d9e7ece25726b7cd54527d938f564950da4
Alex Wissing <awissing123@gmail.com>
2022-11-01 17:11:41 -0600

    added isAdmin middleware to kernel. Modifed authenticate to properly behavior with API requests as opposed to web requests

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -6,16 +6,12 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
-    {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+    protected function unauthenticated($request, array $guards) {
+        if (!$request->expectsJson())
+            return abort(404);
+        abort(response()->json([
+            'response' => 'Invalid Token.',
+            'success' => false
+        ], 401));
     }
 }
