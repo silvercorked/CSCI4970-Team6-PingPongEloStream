318f90768cb1c7b313fa823884af90a0e3febb8e
Alex Wissing <awissing123@gmail.com>
2022-11-08 18:24:56 -0600

    added some basic comments which are then used by doctum to add context and descriptions to values in the docuemntation. These few (seasonalelo.php model and controller.php controller) should be examples for creating more documentation.

--- generate-docs/config.php
+++ generate-docs/config.php
@@ -1,19 +1,31 @@
 <?php
 
 use Doctum\Doctum;
+use Doctum\RemoteRepository\GitHubRemoteRepository;
 use Symfony\Component\Finder\Finder;
 
+$dir = '../';
 $iterator = Finder::create()
-    ->files()
-    ->name('*.php')
-    ->exclude('.git')
-    ->exclude('.vagrant')
-    ->exclude('lang')
-    ->exclude('resources')
-    ->exclude('documentation')
-    ->exclude('storage')
-    ->exclude('vendor')
-    ->in('../');
+	->files()
+	->name('*.php')
+	->exclude('.git')
+	->exclude('.vagrant')
+	->exclude('lang')
+	->exclude('resources')
+	->exclude('documentation')
+	->exclude('storage')
+	->exclude('vendor')
+	->in($dir);
 
-return new Doctum($iterator);
+return new Doctum($iterator, [
+	'title'			=> 'CSCI 4970 Ping Pong Elo Stream API',
+	'language'		=> 'en',
+	'build_dir'		=> __DIR__ . '/../docs',
+	'cache_dir'		=> __DIR__ . '/../docs/cache',
+	'source_dir'		=> dirname($dir),
+	'remote_repository'	=> new GitHubRemoteRepository(
+		'silvercorked/CSCI4970-Team6-PingPongEloStream',
+		dirname($dir)
+	),
+]);
 
---
+++ generate-docs/readme.md
@@ -0,0 +1,3 @@
+## To Update API documentation
+ - run `php doctum.phar update config.php` in this folder.
+
